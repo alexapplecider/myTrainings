998491d1ef3cb6852f555017faa0834f05d67423
alexapplecider <alex.applecider@gmail.com>
2018-02-14 23:19:48 +0200

    commit 2

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,8 +1,8 @@
 window.onload = function () {
 
-  var mainInputVal = document.getElementById("main-input");
-  var itemCount = 0;
-  var list = document.querySelector(".item-list");
+  let mainInputVal = document.getElementById("main-input");
+  let itemCount = 0;
+  let list = document.querySelector(".item-list");
 
   // Клик по кнопке ОК
   document.getElementById("btn-ok").onclick = function () {
@@ -25,17 +25,17 @@ window.onload = function () {
 
   function checkCondition() {
     if (itemCount > 0) {
-      var itemsInList = document.querySelectorAll("li.item > input[type='checkbox']");
-      for (var i = 0; i < itemCount; i++) {
+      let itemsInList = document.querySelectorAll("li.item > input[type='checkbox']");
+      for (let i = 0; i < itemCount; i++) {
 
         itemsInList[i].onclick = function () {
           this.setAttribute("checked", "checked");
-          var label = this.parentElement.querySelector("label");
+          let label = this.parentElement.querySelector("label");
 
           if (this.click && this.hasAttribute("checked")) {
             label.classList.toggle("item-done");
           }
-          if(this.type === "text") {
+          if (this.type === "text") {
             label.classList.remove("item-done");
           }
         }
@@ -45,36 +45,37 @@ window.onload = function () {
   }
 
   function addTask(text) {
-    var itemList = document.createElement("li");
+    let itemList = document.createElement("li");
     itemList.classList.add("item");
-    // Остальные элементы айтема
-    itemList.innerHTML =
-      '<input type="checkbox" id="item-' + itemCount + '">' + "\n" +
-      '<label for="item-' + itemCount + '">' + text + '</label>' +
-      '<button id="btn-save">Сохр.</button><div class="buttons"><button id="btn-delete">Удалить</button><button id="btn-red">Ред.</button></div>';
-
+    // Остальные элементы айтема `
+      itemList.innerHTML =
+            `<input type="checkbox" id="item-${itemCount}">
+            <label for="item-${itemCount}">${text}</label>
+            <button id="btn-save">Сохр.</button>
+            <div class="buttons">
+              <button id="btn-delete">Удалить</button>
+              <button id="btn-red">Ред.</button>
+            </div>`;
     list.insertAdjacentElement("beforeEnd", itemList);
     itemCount++;
   }
 
   function initButtons() {
-    var items = document.querySelectorAll("li.item");
-
-    for (var i = 0; i < items.length; i++) {
+    let items = document.querySelectorAll("li.item");
+    for (let i = 0; i < items.length; i++) {
       items[i].querySelector("#btn-delete").onclick = deleteItem;
       items[i].querySelector("#btn-red").onclick = redItem;
     }
-
+    
     function deleteItem() {
       this.parentElement.parentElement.remove();
     }
-
     function redItem() {
-      var input = this.parentElement.parentElement.querySelector("input");
-      var label = this.parentElement.parentElement.querySelector("label");
-      var btnSave = this.parentElement.parentElement.querySelector("#btn-save");
+      let input = this.parentElement.parentElement.querySelector("input");
+      let label = this.parentElement.parentElement.querySelector("label");
+      let btnSave = this.parentElement.parentElement.querySelector("#btn-save");
       btnSave.style.display = "block";
-      var btnRed = this;
+      let btnRed = this;
       btnRed.style.display = "none";
 
       btnSave.onclick = function () {
